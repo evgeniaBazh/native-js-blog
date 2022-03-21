9d3d2f3b1f0213693fe67894956934805a41d5f0
evgeniaBazh <evgeniabazh7@gmail.com>
2022-03-21 17:28:50 +0500

    Компоненты карточек постов

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Название компонентов пишется с заглавной буквы
+
 const links = [
     'Посты',
     'Создать'
@@ -27,3 +29,69 @@ function Header(arr) {
 }
 
 document.body.append(Header(links));
+
+const cards = [
+    {
+        img: 'https://firebasestorage.googleapis.com/v0/b/alyle-ui.appspot.com/o/img%2FMountains-Blue.jpg?alt=media&token=d04f0279-79c6-4752-8b5a-cccd73720243',
+        header: 'Blue Mountains',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae laboriosam ipsa animi alias rem.',
+    },
+    {
+        img: 'https://photocasa.ru/uploads/posts/2017-01/1485218882_img_5932.jpg',
+        header: 'Sunflowers',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae laboriosam ipsa animi alias rem.',
+    },
+    {
+        img: 'https://vkrym.su/Images/Get/638',
+        header: 'Sea',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae laboriosam ipsa animi alias rem.',
+    },
+    {
+        img: 'https://funart.pro/uploads/posts/2021-04/1618436301_53-funart_pro-p-oboi-fon-kosmos-zvezdi-54.jpg',
+        header: 'Cosmos',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae laboriosam ipsa animi alias rem.',
+    }
+]
+
+function Card(props) {
+    const card = document.createElement('div');
+    const media = document.createElement('img');
+    const content = document.createElement('div');
+    const headLine = document.createElement('h2');
+    const text = document.createElement('p');
+
+    card.className = 'posts__card';
+    media.className = 'posts__media';
+    content.className = 'posts__content';
+    headLine.className = 'posts__headLine';
+    text.className = 'posts__text'
+
+    media.src = props.img;
+    headLine.textContent = props.header;
+    text.textContent = props.text;
+
+    card.appendChild(media);
+    card.appendChild(content);
+    content.appendChild(headLine);
+    content.appendChild(text);
+
+    return card;
+}
+
+function Cards(cards) {
+    const posts = document.createElement('div');
+    const container = document.createElement('div');
+
+    posts.className = 'posts';
+    container.className = 'container';
+
+    posts.appendChild(container);
+
+    cards.forEach((card) => {
+        container.append(Card(card));
+    })
+
+    return posts;
+}
+
+document.body.append(Cards(cards));
